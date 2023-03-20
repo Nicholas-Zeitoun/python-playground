882963366343deec78130b7bda7c906480d95962
NicholasZeitoun <nzeit8003@gmail.com>
2023-03-20 20:25:11 +1100

    escaping quotes in strings

--- stringsss.py
+++ stringsss.py
@@ -1,2 +1,6 @@
 print('spam eggs')
 print('doesn\'t') # use \' to escape the single quote...
+print("doesn't") # use doublequotes instead
+print('"Yes," they said.')
+print("\"Yes,\" they said.")
+print('"Isn\'t," they said.')
