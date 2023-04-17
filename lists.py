ecf64f5d895e4e72870930234a3fa9be981e1cb9
NicholasZeitoun <nzeit8003@gmail.com>
2023-04-17 22:39:23 +1000

    All slice and concat

--- lists.py
+++ lists.py
@@ -3,3 +3,6 @@ print(squares)
 print(squares[0]) # indexing returns the item
 print(squares[-1])
 print(squares[-3:]) # slicing returns a new list
+print(squares[:]) # All slice. returns shallow copy of the list
+squares += [36, 49, 64, 81, 100] # list concatenation
+print(squares)
