00cbc273b24268c49998dbc7aa0dab6409e30a51
NicholasZeitoun <nzeit8003@gmail.com>
2023-04-18 20:34:33 +1000

    Lists aren't immutable

--- lists.py
+++ lists.py
@@ -6,3 +6,7 @@ print(squares[-3:]) # slicing returns a new list
 print(squares[:]) # All slice. returns shallow copy of the list
 squares += [36, 49, 64, 81, 100] # list concatenation
 print(squares)
+cubes = [1, 8, 27, 65, 125] # somethigns wrong here!
+print(cubes)
+cubes[3] = 64 # lists are not immutable
+print(cubes)
