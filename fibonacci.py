9083f3fb241f747778a7ef3ec7d92af3988e7a59
NicholasZeitoun <nzeit8003@gmail.com>
2023-04-26 23:44:46 +1000

    keyword argument end

--- fibonacci.py
+++ fibonacci.py
@@ -7,3 +7,9 @@ while a < 10:
 
 i = 256*256
 print('The value of i is', i)
+
+# keyword argument end can be used to avoid the newline after the output. Or end input with a different string
+a, b = 0, 1
+while a < 1000:
+    print(a, end=',')
+    a, b = b, a+b
